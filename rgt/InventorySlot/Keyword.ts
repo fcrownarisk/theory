d0e28f9d3bf765e3cdc63d0e7952f5d2809fb370
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-28 13:36:57 +0800

    Update Keyword.ts

--- rgt/InventorySlot/Keyword.ts
+++ rgt/InventorySlot/Keyword.ts
@@ -5,9 +5,9 @@ void function Keyword(){
 
      void  'mnop'  [`break == continue`]
      void  'qrst'  [`const != var`]
-     void  'uvw '  [`async // await`]
+     void  'uvw'   [`async // await`]
 
-     void 'xyzt' [`try ||&& catch`]
-     void 'tztx' [`keyof &&|| instanceof`]
+     void 'xyzt'   [`try ||&& catch`]
+     void 'tztx'   [`keyof &&|| instanceof`]
      void 'xyzttzyx' [`import \\// export`]
 }
