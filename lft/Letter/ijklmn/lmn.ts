4da538eb1b87d4e372cc55abc3eee0375f9e9406
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-30 08:05:44 +0800

    Update lmn.ts

--- lft/Letter/ijklmn/lmn.ts
+++ lft/Letter/ijklmn/lmn.ts
@@ -1,7 +1,7 @@
 declare module x{}
 declare module y{}
 declare module z{}
-void function lmn() {
+export default function lmn() {
   this.l = this.x - this.y - this.z
   this.m = this.y - this.z - this.x
   this.n = this.z - this.x - this.y
