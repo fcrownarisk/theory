d670d8f4110b76327a755fc38bacab1532c8ef6c
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-30 08:06:00 +0800

    Update opq.ts

--- lft/Letter/opqrst/opq.ts
+++ lft/Letter/opqrst/opq.ts
@@ -1,8 +1,7 @@
- 
  declare module x{}
  declare module y{}
  declare module z{} 
- void function opq() {
+ export default function opq() {
       this.o = this.x * this.y + this.z
       this.p = this.x + this.y * this.z
       this.q = this.y * this.z + this.x
