a55433768287987292f582001f94adffe968d89a
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-30 08:06:14 +0800

    Update rst.ts

--- lft/Letter/opqrst/rst.ts
+++ lft/Letter/opqrst/rst.ts
@@ -1,7 +1,7 @@
  declare module x{}
  declare module y{}
  declare module z{} 
- void function rst(){
+export default function rst(){
    this.r = (this.x / this.y) / this.z
    this.s = this.y / (this.x / this.z)
    this.t = (this.z / this.x) / this.y
