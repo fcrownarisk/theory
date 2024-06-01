e0bbf649c2534b7d2389e54fc03723725691cd9b
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-01 17:10:14 +0800

    Update odd.js

--- lft/Collatz/air/odd.js
+++ lft/Collatz/air/odd.js
@@ -2,7 +2,7 @@ import '../mobius'
 import '../tree/tree_i'
 import '../tree/tree_ii'
 export function odd(){
-    void function OddNumber(oddNumber){
+    void function OddNumber(odd){
         if (oddNumber % 2 === 0) {
             oddNumber = oddNumber + 1;
         } else {
