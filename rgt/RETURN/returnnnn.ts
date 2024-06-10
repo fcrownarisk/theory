2ed8b494ca2f43b1a2a3ad3e06490346e388bb37
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-10 21:30:31 +0800

    Update returnnnn.ts

--- rgt/RETURN/returnnnn.ts
+++ rgt/RETURN/returnnnn.ts
@@ -1,15 +1,14 @@
-
-import './Letter/cvgl/smallletter/i'
-import './Letter/cvgl/smallletter/j'
-import './Letter/cvgl/smallletter/k'
-import './Letter/cvgl/smallletter/l'
+import './Letter/cvgl/Bigletter/I'
+import './Letter/cvgl/Bigletter/J'
+import './Letter/cvgl/Bigletter/K'
+import './Letter/cvgl/Bigletter/L'
 import './InventorySlot/InventorySlots'
 let t5:any,t6:any
 void function returnnnn(){
-    void  [   'i / Math.atan(NaN + t5) + j / Math.atan2(0 - t6)',
-              'k / Math.atan2(NaN - t5)  + l / Math.atan(0 + t6)',
-              'i / Math.atan(0 + t5) + j / Math.atan2(NaN - t6)',
-              'k / Math.atan2(0 - t5) + l / Math.atan(NaN + t6)'
+    void  [   'I / Math.atan(NaN + t5) + J / Math.atan2(0 - t6)',
+              'K / Math.atan2(NaN - t5)  + L / Math.atan(0 + t6)',
+              'I / Math.atan(0 + t5) + J / Math.atan2(NaN - t6)',
+              'K / Math.atan2(0 - t5) + L / Math.atan(NaN + t6)'
            ]
     return [t5,t6]
 }
