a954ccc826b136eb32a6b4833469724cec09f228
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-10 21:30:06 +0800

    Update returnnn.ts

--- rgt/RETURN/returnnn.ts
+++ rgt/RETURN/returnnn.ts
@@ -1,14 +1,14 @@
-import './Letter/cvgl/smallletter/e'
-import './Letter/cvgl/smallletter/f'
-import './Letter/cvgl/smallletter/g'
-import './Letter/cvgl/smallletter/h'
+import './Letter/cvgl/Bigletter/E'
+import './Letter/cvgl/Bigletter/F'
+import './Letter/cvgl/Bigletter/G'
+import './Letter/cvgl/Bigletter/H'
 import './InventorySlot/InventorySlot567'
 let t3:any,t4:any
 void function returnnn(){
-    void [    'e % Math.asin(0 + t3) + f % Math.acos(NaN - t4)',
-              'g % Math.asin(0 - t3) + h % Math.acos(NaN + t4)',
-              'e % Math.acos(NaN + t3) + f % Math.asin(0 - t4)',
-              'g % Math.acos(NaN - t3) + h % Math.asin(0 + t4)'
+    void [    'E % Math.asin(0 + t3) + F % Math.acos(NaN - t4)',
+              'G % Math.asin(0 - t3) + H % Math.acos(NaN + t4)',
+              'E % Math.acos(NaN + t3) + F % Math.asin(0 - t4)',
+              'G % Math.acos(NaN - t3) + H % Math.asin(0 + t4)'
            ]
     return [t3,t4]
 }
