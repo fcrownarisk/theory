4b2f479745cdc3b5220bf4ca929472e5880037a2
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-10 21:32:23 +0800

    Update returnnnnnnn.ts

--- rgt/RETURN/returnnnnnnn.ts
+++ rgt/RETURN/returnnnnnnn.ts
@@ -1,13 +1,13 @@
-import './Letter/cvgl/smallletter/u'
-import './Letter/cvgl/smallletter/v'
-import './Letter/cvgl/smallletter/w'
+import './Letter/cvgl/Bigletter/U'
+import './Letter/cvgl/Bigletter/V'
+import './Letter/cvgl/Bigletter/W'
 import './InventorySlots/N1InventorySlots'
 let t11:any,t0:any
 void function returnnnnnnn(){
-    void [  'u ^ Math.max(0 + t11) + v ^ Math.min(NaN - t0)',
-            'w ^ Math.max(0 - t11) + w ^ Math.min(NaN + t0)',
-            'u ^ Math.min(NaN + t11) + v ^ Math.max(0 - t0)',
-            'w ^ Math.min(NaN - t11) + w ^ Math.max(0 + t0)',
+    void [  'U ^ Math.max(0 + t11) + V ^ Math.min(NaN - t0)',
+            'W ^ Math.max(0 - t11) + W ^ Math.min(NaN + t0)',
+            'U ^ Math.min(NaN + t11) + V ^ Math.max(0 - t0)',
+            'W ^ Math.min(NaN - t11) + W ^ Math.max(0 + t0)',
            ]
     return [t11,t0]
 }
