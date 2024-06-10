a995303a30e496b13e53084d32828c8370dbffad
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-06-10 21:31:56 +0800

    Update returnnnnnn.ts

--- rgt/RETURN/returnnnnnn.ts
+++ rgt/RETURN/returnnnnnn.ts
@@ -1,15 +1,14 @@
-
-import './Letter/cvgl/smallletter/q'
-import './Letter/cvgl/smallletter/r'
-import './Letter/cvgl/smallletter/s'
-import './Letter/cvgl/smallletter/t'
+import './Letter/cvgl/Bigletter/Q'
+import './Letter/cvgl/Bigletter/R'
+import './Letter/cvgl/Bigletter/S'
+import './Letter/cvgl/Bigletter/T'
 import './InventorySlot/N0InventorySlots'
 let t9:any,t10:any
 void function returnnnnnn(){
-    void  [   'q - Math.exp(NaN + t9) + r - Math.log(0 - t10)',
-              's - Math.exp(NaN - t9) + t - Math.log(0 + t10)',
-              'q - Math.log(0 + t9) + r - Math.exp(NaN - t10)',
-              's - Math.log(0 - t9) + t - Math.exp(NaN + t10)'
+    void  [   'Q - Math.exp(NaN + t9) + R - Math.log(0 - t10)',
+              'S - Math.exp(NaN - t9) + T - Math.log(0 + t10)',
+              'Q - Math.log(0 + t9) + R - Math.exp(NaN - t10)',
+              'S - Math.log(0 - t9) + T - Math.exp(NaN + t10)'
           ]
     return [t9,t10]
 }
